de647e5509e30ae9a366bd0940fd99b57417cf03
OudomMunint <oudommunint@gmail.com>
2025-06-21 01:50:05 +1000

    #2 - Implement event to run hashing bench.

--- Pages/BenchmarkPage.xaml.cs
+++ Pages/BenchmarkPage.xaml.cs
@@ -1,9 +1,36 @@
+using System.Collections.Generic;
+
 namespace MauiBench.Pages;
 
 public partial class BenchmarkPage : ContentPage
 {
-	public BenchmarkPage()
-	{
-		InitializeComponent();
-	}
+    public List<string> Results { get; set; } = new List<string>();
+
+    int count = 0;
+
+    public BenchmarkPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void HashingBenchmarkButton_Clicked(object sender, EventArgs e)
+    {
+        HashingSpinner.IsRunning = true;
+        HashingSpinner.IsVisible = true;
+        string thisbenchmarkResults = "";
+        Benchmarks.HashingBenchmark? hashBenchmark = null;
+
+        await Task.Run(() =>
+        {
+            hashBenchmark = new Benchmarks.HashingBenchmark();
+            thisbenchmarkResults = hashBenchmark.CombinedHashingExport();
+        });
+
+        Results.Add(thisbenchmarkResults);
+        ResultsLabel.Text = thisbenchmarkResults;
+        HashingSpinner.IsRunning = false;
+        HashingSpinner.IsVisible = false;
+
+        hashBenchmark?.DisposeOf();
+    }
 }
