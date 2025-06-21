38ab4ebdb077546aeb6c08b79f145609835b0281
OudomMunint <oudommunint@gmail.com>
2025-06-22 00:34:12 +1000

    #3 - Set up DB & DB constants.

---
+++ Constants.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiBench
+{
+    public static class Constants
+    {
+        public const string DatabaseFileName = "MauiBench.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache |
+            SQLite.SQLiteOpenFlags.Uri;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFileName);
+            }
+        }
+    }
+}
---
+++ Data/ItemDatabase.cs
@@ -0,0 +1,40 @@
+﻿using MauiBench.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBench.Data
+{
+    public class ItemDatabase
+    {
+        static SQLiteAsyncConnection? Database;
+
+        public static readonly AsyncLazy<ItemDatabase> Instance =
+            new AsyncLazy<ItemDatabase>(async () =>
+            {
+                var instance = new ItemDatabase();
+                try
+                {
+                    if (Database == null)
+                    {
+                        throw new InvalidOperationException("Database connection is not initialized.");
+                    }
+
+                    CreateTableResult result = await Database.CreateTableAsync<BenchmarkModel>();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                return instance;
+            });
+
+        public ItemDatabase()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+    }
+}
