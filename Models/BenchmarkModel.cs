78d7d30ee71ca34a8c3ac5ef670dcd93759a1d6a
OudomMunint <oudommunint@gmail.com>
2025-06-22 00:34:53 +1000

    #3 - Implement data model for benchmark items.

---
+++ Models/BenchmarkModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBench.Models
+{
+    public class BenchmarkModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        
+        public DateTime Timestamp { get; set; }
+
+        public string TestName { get; set; }
+
+        public Type BenchmarkType { get; set; }
+
+        public string Result { get; set; }
+
+        public enum Type
+        {
+            Partial,
+            Full
+        }
+    }
+}
