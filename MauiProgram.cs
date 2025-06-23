11a42d2b83c9a529af46b116b8ccf79f16fec5b0
OudomMunint <oudommunint@gmail.com>
2025-06-23 22:18:21 +1000

    Use mctk

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MauiBench
 {
@@ -8,7 +9,7 @@ namespace MauiBench
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
+                .UseMauiApp<App>().UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
