b8e1acc4c2e1d8073cdbe57cc06fb7650b6fcf46
OudomMunint <oudommunint@gmail.com>
2025-06-29 23:07:15 +1000

    Set icon based on platform.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,6 +5,19 @@
         public AppShell()
         {
             InitializeComponent();
+
+            if (OperatingSystem.IsWindows())
+            {
+                BenchmarkPage.Icon = "speedom.png";
+                ResultsPage.Icon = "results.png";
+                SystemInfo.Icon = "info.png";
+            }
+            else
+            {
+                BenchmarkPage.Icon = null;
+                ResultsPage.Icon = null;
+                SystemInfo.Icon = null;
+            }
         }
     }
 }
